1eceab363e7b02df73bb5da138ff40dadd56f1b9
bobcastaldeli <castaldelliroberto@gmail.com>
2021-08-30 23:32:37 -0300

    ajust basic_cleaning

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -33,6 +33,9 @@ def go(args):
     df = df[idx].copy()
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     logger.info(f'Save clean csv {args.output_artifact}')
     df.to_csv("clean_sample.csv", index=False)
 
